d13e373f13c8bb0ee71eaf615d6dc8cb7148395d
Jimmy neutron <qny123@naver.com>
2022-08-26 20:52:41 +0900

    chore: 아이콘 svg파일로 추가

---
+++ src/components/icons/LeftArrowIcon.tsx
@@ -0,0 +1,15 @@
+function LeftArrow() {
+  return (
+    <svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M8.07107 1.92893L1 9L8.07107 16.0711"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default LeftArrow;
