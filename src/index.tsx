5bf30f9dfc57412666d18598755c000fb6c4341d
Jimmy neutron <qny123@naver.com>
2022-08-26 20:54:23 +0900

    style: theme provider 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/DefaultStyle";
+import GlobalStyle from "./styles/GlobalStyle";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
+import CloseIcon from "../components/icons/CloseIcon";
+import styled from "styled-components";
+
 function Home() {
-  return (
-    <p>Home</p>
-  )
+  return <HomeWrapper></HomeWrapper>;
 }
+
+const HomeWrapper = styled.div`
+  width: 390px;
+  height: 1322px;
+  background: ${({ theme: { colors } }) => colors.white};
+  margin: auto;
+`;
 
 export default Home;
