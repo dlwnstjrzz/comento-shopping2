41eb142e2c5119ee2cda063496532a508c294220
Jimmy neutron <qny123@naver.com>
2022-08-26 18:43:16 +0900

    router 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home, Basket, Product } from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <p>test test</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/basket" element={<Basket />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import Home from "./Home";
+import Basket from "./Baskets";
+import Product from "./Product";
+
+export { Home, Basket, Product };
