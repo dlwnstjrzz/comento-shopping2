399b411964deaab52e1c467cada8689dc5c1df68
Jimmy neutron <qny123@naver.com>
2022-08-28 23:55:08 +0900

    feat: header컴포넌트 분리 후 home page에 추가

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { LogoBar } from "./LogoBar";
+import { HashTagItems } from "./HashTagItems";
+
+export function Header() {
+  return (
+    <div>
+      <LogoBar />
+      <HashTagItems />
+    </div>
+  );
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { Header } from "components/Header";
+import { ItemList } from "components/ItemList";
+
+function Home() {
+  return (
+    <HomeWrapper>
+      <Header />
+      <ItemList />
+    </HomeWrapper>
+  );
+}
+
+const HomeWrapper = styled.div`
+  width: 390px;
+  height: 1322px;
+  background: ${({ theme: { colors } }) => colors.white};
+  margin: auto;
+`;
+
+export default Home;
