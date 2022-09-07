583a9000b87f2dbcc333198609eb38da8295d1e1
Jimmy neutron <qny123@naver.com>
2022-09-08 02:32:51 +0900

    chore: 라우터 주소 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home, Basket, Product } from "./pages";
+import { Home, Basket, ProductDetail } from "./pages";
 
 function App() {
   console.log(process.env.PUBLIC_URL);
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="" element={<Home />} />
-        <Route path="/product" element={<Product />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
         <Route path="/basket" element={<Basket />} />
       </Routes>
     </BrowserRouter>
