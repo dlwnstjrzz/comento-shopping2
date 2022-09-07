fdf0134e27c2047c0b32f59a29d58ad3d5ab9d23
Jimmy neutron <qny123@naver.com>
2022-09-08 02:36:44 +0900

    chore: 목데이터 구조 객체에서 배열로 변경

--- src/mocks/itemInfo.js
+++ src/mocks/itemInfo.js
@@ -1,46 +1,61 @@
-export const itemInfo = {
-  "#겨울 방한템": [
-    {
-      imgUrl:
-        "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product1.jpg",
-      title: "비숑 블랙 머그잔",
-      description: "쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 블랙 & 화이트 비숑 머그잔입니다.",
-    },
-    {
-      imgUrl:
-        "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product2.jpg",
-      title: "가열 보온 티코스터 온열 원터치 컵 받침대",
-      description:
-        "언제나 따뜻하게 최대 12시간 동안 최대 60도의 온도로 따듯한 차를 즐길 수 있습니다.",
-    },
-    {
-      imgUrl:
-        "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product3.jpg",
-      title: "벨루즈까사 솜사탕 파스텔 머그 4종 세트",
-      description:
-        "솜사탕처럼 부드러운 쉐입에 스트라이프 조각이 더해져 심플하면서도 세련된 파스텔 컬러의 머그",
-    },
-  ],
-  "#따순머그컵": [
-    {
-      imgUrl:
-        "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product1.jpg",
-      title: "테스트1",
-      description: "쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 블랙 & 화이트 비숑 머그잔입니다.",
-    },
-    {
-      imgUrl:
-        "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product2.jpg",
-      title: "테스트2",
-      description:
-        "언제나 따뜻하게 최대 12시간 동안 최대 60도의 온도로 따듯한 차를 즐길 수 있습니다.",
-    },
-    {
-      imgUrl:
-        "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product3.jpg",
-      title: "테스트3",
-      description:
-        "솜사탕처럼 부드러운 쉐입에 스트라이프 조각이 더해져 심플하면서도 세련된 파스텔 컬러의 머그",
-    },
-  ],
-};
+export const itemInfo = [
+  {
+    id: 1,
+    imgUrl:
+      "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product1.jpg",
+    title: "비숑 블랙 머그잔",
+    description: "쌀쌀한 날씨에 따뜻한 우유,\n 커피 한잔하기 좋은 블랙 & 화이트 비숑 머그잔입니다.",
+    price: 21800,
+    hashTag: "#겨울 방한템",
+  },
+  {
+    id: 2,
+    imgUrl:
+      "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product2.jpg",
+    title: "가열 보온 티코스터 온열 원터치 컵 받침대",
+    description:
+      "언제나 따뜻하게 최대 12시간 동안 최대 60도의 온도로 따듯한 차를 즐길 수 있습니다.",
+    price: 21800,
+    hashTag: "#겨울 방한템",
+  },
+  {
+    id: 3,
+    imgUrl:
+      "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product3.jpg",
+    title: "벨루즈까사 솜사탕 파스텔 머그 4종 세트",
+    description:
+      "솜사탕처럼 부드러운 쉐입에 스트라이프 조각이 더해져 심플하면서도 세련된 파스텔 컬러의 머그",
+    price: 21800,
+    hashTag: "#겨울 방한템",
+  },
+
+  {
+    id: 4,
+    imgUrl:
+      "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product1.jpg",
+    title: "테스트1",
+    description: "쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 블랙 & 화이트 비숑 머그잔입니다.",
+    price: 21800,
+    hashTag: "#따순머그컵",
+  },
+  {
+    id: 5,
+    imgUrl:
+      "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product2.jpg",
+    title: "테스트2",
+    description:
+      "언제나 따뜻하게 최대 12시간 동안\n최대 60도의 온도로 따듯한 차를 즐길 수 있습니다.",
+    price: 21800,
+    hashTag: "#따순머그컵",
+  },
+  {
+    id: 6,
+    imgUrl:
+      "https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product3.jpg",
+    title: "테스트3",
+    description:
+      "솜사탕처럼 부드러운 쉐입에 스트라이프 조각이 더해져 심플하면서도 세련된 파스텔 컬러의 머그",
+    price: 21800,
+    hashTag: "#따순머그컵",
+  },
+];
