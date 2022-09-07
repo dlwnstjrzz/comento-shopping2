2e276cbc5d904bec7c4f08f5be7d8fef290023fe
Jimmy neutron <qny123@naver.com>
2022-09-08 02:35:21 +0900

    feat: 상품 클릭시 navigate를 활용해 페이지 이동

--- src/components/ItemList/index.tsx
+++ src/components/ItemList/index.tsx
@@ -2,14 +2,38 @@ import styled from "styled-components";
 import { Item } from "./Item";
 import { itemInfo } from "mocks/itemInfo";
 import { HashTagItemsContextStore } from "context/HashTagProvider";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import { getProductByHashTag } from "utils/utils";
+import { useNavigate } from "react-router-dom";
+
+interface IitemInfo {
+  imgUrl: string;
+  description: string;
+  title: string;
+  id: number;
+}
 
 export function ItemList() {
   const { hashTagTitle } = useContext(HashTagItemsContextStore);
+  const navigate = useNavigate();
+  const [itemList, setItemList] = useState(getProductByHashTag(hashTagTitle, itemInfo));
+
+  useEffect(() => {
+    setItemList(getProductByHashTag(hashTagTitle, itemInfo));
+  }, [hashTagTitle]);
+  //질문 product가 왜 불린인가
   return (
     <ItemListWrapper>
-      {itemInfo[hashTagTitle].map((el: { imgUrl: string; description: string; title: string }) => {
-        return <Item imgUrl={el.imgUrl} description={el.description} title={el.title} />;
+      {itemList.map((product: IitemInfo) => {
+        return (
+          <Item
+            onClick={() => navigate(`product/${product.id}`)}
+            key={product.id}
+            imgUrl={product.imgUrl}
+            description={product.description}
+            title={product.title}
+          />
+        );
       })}
     </ItemListWrapper>
   );
